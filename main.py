cdac3e83d6498e25cd11f80af944682cd24a70a9
amyote <144702416+amyote@users.noreply.github.com>
2023-10-12 16:08:30 -0400

    Update main.py

--- main.py
+++ main.py
@@ -3,6 +3,7 @@ import random
 
 
 continuer = True
+initNouvelleRonde = True
 
 niveauVie = 20
 numeroAdversaire = 0
@@ -17,6 +18,7 @@ forceMaxDe = 5
 forceBoss = 10
 
 
+# Montrer le statut pre=combat a l'utilisateur.
 def print_status():
     print(f"STATUT PRE-COMBAT:\n\
 Adversaire: {numeroAdversaire}\n\
@@ -24,19 +26,24 @@ Force de l'adversaire: {forceAdversaire}\n\
 Niveau de vie de l'usager: {niveauVie}\n\
 Combat {numeroCombat}: {nombreVictoire} vs {nombreDefaite}")
 
+
 while continuer:
     
     input("\n\nAppuyez sur Entree pour continuer/commencer...")
 
 
-    numeroAdversaire += 1
+    # Si on devrait initialiser une nouvelle ronde. En particulier, on ne devrait pas s'ils choisissent de voir les regles.
+    if initNouvelleRonde:
 
-    if nombreVictoireSuite >= 3:
-        forceAdversaire = forceBoss
-    else:
-        # On a une distribution differente entre chiffre au hasard entre 1 et 12 et somme de deux chiffres au hasard entre 1 et 6
-        forceAdversaire = random.randint(forceMinDe, forceMaxDe) + random.randint(forceMinDe, forceMaxDe)
+        numeroAdversaire += 1
 
+        if nombreVictoireSuite >= 3:
+            forceAdversaire = forceBoss
+        else:
+            # On a une distribution differente entre chiffre au hasard entre 1 et 12 et somme de deux chiffres au hasard entre 1 et 6
+            forceAdversaire = random.randint(forceMinDe, forceMaxDe) + random.randint(forceMinDe, forceMaxDe)
+
+    # Dire a l'utilisateur sa situation et ses options.
     print(f"Vous tombez face a face avec un adversaire de difficulte: {forceAdversaire}.")
     entree = input("Que voulez-vous faire?\n\
 1. Combattre cet adversaire\n\
@@ -46,6 +53,7 @@ while continuer:
 : ")
 
 
+    # S'il choisit de se battre, on affiche le statut, roule le de du joueur et on compare a la force de l'adversaire.
     if entree == "1":
 
         print_status()
@@ -59,6 +67,7 @@ while continuer:
 Lancer des: {resultatDes}\n\
 Dernier combat: {'victoire' if victoireCombat else 'defaite'}")
 
+        # Si on gagne, augmenter le nombre de victoire, le nombre de victoires de suite et le niveau de vie.
         if victoireCombat:
 
             nombreVictoire += 1
@@ -69,6 +78,7 @@ Dernier combat: {'victoire' if victoireCombat else 'defaite'}")
 Niveau de vie: {niveauVie}\n\
 Nombre de victoires consecutives: {nombreVictoireSuite}")
     
+        # Si on perd, augmenter le nombre de defaites, annuler le nombre de victoires de suite et diminuer le niveau de vie.
         else:
 
             nombreDefaite += 1
@@ -78,14 +88,19 @@ Nombre de victoires consecutives: {nombreVictoireSuite}")
             print(f"STATUT POST-COMBAT:\n\
 Niveau de vie: {niveauVie}")
 
+            # Verifier si on est mort.
             if niveauVie <= 0:
                 print(f"La partie est terminee, vous avez vaincu {nombreVictoire} monstre(s)")
                 continuer = False
+        initNouvelleRonde = True
     
+    # S'il choisit de ne pas se battre, on diminue le niveau de vie.
     elif entree == "2":
 
         niveauVie -= 1
+        initNouvelleRonde = True
 
+    # S'il veut voir les regles, on lui montre.
     elif entree == "3":
 
         print("REGLES:\n\
@@ -93,8 +108,11 @@ Pour réussir un combat, il faut que la valeur du dé lancé soit supérieure à
 Une défaite a lieu lorsque la valeur du dé lancé par l’usager est inférieure ou égale à la force de l’adversaire.  Dans ce cas, le niveau de vie de l’usager est diminué de la force de l’adversaire.\n\
 La partie se termine lorsque les points de vie de l’usager tombent sous 0.\n\
 L’usager peut combattre ou éviter chaque adversaire, dans le cas de l’évitement, il y a une pénalité de 1 point de vie.")
+
+        initNouvelleRonde = False
         
 
+    # S'il veut quitter, on termine la boucle de jeu.
     elif entree == "4":
 
         print("Merci et au revoir...")
